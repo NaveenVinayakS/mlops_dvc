4d7dd69ea26a398b0f992791ed727b0fa8713f5d
snv1995 <naveenvinayak.2195@gmail.com>
2021-09-08 14:02:59 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+create env
+''' bash
+conda create -n mlops_wineq python=3.7 -y
+'''
+
+activate enviorment
+
+'''bash
+conda activate mlops_wineq
+'''
+
+create a req file
+
+install the requirement
+'''bash
+pip install -r requirements.txt
+'''
+
+Download data from
+
+https://drive.google.com/drive/folders/18zqQiCJVgF7uzXgfbIJ-04zgz1ItNfF5?usp=sharing
+
+git init
+
+dvc init
+
+data add data_given/winewuality.csv
+
---
+++ template.py
@@ -0,0 +1,27 @@
+# instead of cookie cutter library we can use this methord
+import os
+
+dirs = [
+    os.path.join("data","raw"),
+    os.path.join("data","processed"),
+    "notebooks",
+    "saved_models",
+    "src"
+
+]
+
+for dir_ in dirs:
+    os.makedirs(dir_,exist_ok = True)
+    with open(os.path.join(dir_,".gitkeep"),"w") as f:
+        pass
+
+file = [
+    "dvc.yaml",
+    "params.yaml",
+    ".gitignore",
+    os.path.join("src", "__init__.py")
+]
+
+for file_ in file:
+    with open(file_, "w") as f:
+        pass
