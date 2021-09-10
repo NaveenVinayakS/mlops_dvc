7804448f4e20d34141c07c870f607691ff8558a1
snv1995 <naveenvinayak.2195@gmail.com>
2021-09-10 17:43:06 +0530

    pytest example

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,11 @@
+import pytest
+class NotInRange(Exception):
+    def __init__(self,message="value not in range"):
+        self.message = message
+        super().__init__(self.message)
+
+
+
 def test_generic(): # function name must start with TEST
     # assert -> assertion return true(PASSED) if test case PASS.
     # individually we can run this file "pytest -v"
@@ -5,6 +13,13 @@ def test_generic(): # function name must start with TEST
 
     # what tox does is it will create a python virtual enviorment using tox ini file and requirements are
     # collected from -rrequirements.txt , command is "tox" if any changes in requirement the "tox -r"
-    a=3
-    b=3
-    assert a==b
+    a=5
+    with pytest.raises(NotInRange):
+        if a not in range(10,20):
+            raise NotInRange
+    # OR
+    #try:
+    #    if a not in range(10, 20):
+    #        raise NotInRange
+    #except Exception as e:
+    #    print(e)
